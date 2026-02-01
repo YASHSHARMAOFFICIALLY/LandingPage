ff5927cf2f2d435f046c9dbc4af3d5a63899b7ce
Yash sharma <yashsharmaofficially@gmail.com>
2026-02-01 08:18:46 +0530

    card

--- component/features/Index.tsx
+++ component/features/Index.tsx
@@ -2,11 +2,14 @@ import { Container } from "../Container";
 import React from "react";
 import { Heading } from "../heading";
 import { SubHeading } from "../subheading";
-import { Card, CardConetent, CardTitle } from "./card";
+import { Card, CardConetent, CardTitle,CardCTA } from "./card";
+
+import { IconPlus } from '@tabler/icons-react';
+
 export  const Features = () => {
     return (
    <Container className="py-10 md:py-20 lg:py-32">
-    <div className="flex flex-col md:flex-row items-baseline-last justify-between gap-10 lg:gap-30">
+    <div className="flex  items-baseline-last justify-between gap-10 lg:gap-30">
          <Heading>
             Built for Fast Moving <br/> Teams That Need Control 
         </Heading>
@@ -15,23 +18,32 @@ export  const Features = () => {
         </SubHeading>
         </div>
 
-    <div className="grid grid:cols-1 md:grid-cols-3 gap-4">           
-        <Card>
-            <CardConetent>
+    <div className="grid grid:cols-1 md:grid-cols-3 gap-4 my-10 md:my-20 ">           
+        <Card className="rounded-tl-3xl rounded-bl-3xl">
+            <CardConetent className="flex items-center justify-between gap-4 p-0"> 
                  <CardTitle>Prebuilt Agents,Tuned to Your Workflows</CardTitle>
+                 <CardCTA>
+                     <IconPlus/>
+                 </CardCTA>
             </CardConetent>
         </Card>
 
-        <Card>
-            <CardConetent>
+        <Card className="rounded-tl-3xl rounded-bl-3xl">
+            <CardConetent className="flex items-center justify-between gap-4 p-0">
                  <CardTitle>Automate Handoffs,Reduce Ops Friction</CardTitle>
+                 <CardCTA>
+                     <IconPlus/>
+                 </CardCTA>
             </CardConetent>  
         </Card>
 
 
-        <Card>
-            <CardConetent>
-                <CardTitle>Approvals,Guardrails,and Full Auditablity</CardTitle>
+        <Card className="rounded-tl-3xl rounded-bl-3xl overflow-hidden max-w-full">
+            <CardConetent className="flex items-center justify-between gap-4 p-0">
+                <CardTitle className="leading-tight flex-1">Approvals,Guardrails,and Full Auditablity</CardTitle>
+                <CardCTA className="shrink-0">
+                     <IconPlus/>
+                 </CardCTA>
             </CardConetent>
             
         </Card>
--- component/features/card.tsx
+++ component/features/card.tsx
@@ -15,7 +15,7 @@ export const CardTitle  = ({className,children}:{
     className?:  string,
     children:React.ReactNode;
 })=>{
-    return <h3 className= {cn("text-lg  md:text-2xl  font-display font-medium",className)}>
+    return <h3 className= {cn("text-lg  md:text-2xl  font-display font-bold",className)}>
         {children}
     </h3>
 }
@@ -36,9 +36,34 @@ export const CardConetent = ({
 
 }) =>{
     return( 
-    <div className= {cn("px-4 pb-6",className)}>
+    <div className= {cn("px-4 pb-6 md:pb-12",className)}>
         {children}
     </div>
     )
 }
 
+export const CardCTA =  ({
+    className,
+    children,
+    ...rest
+}:React.ComponentProps<"button">)=>{
+    return(
+        <button
+        className={cn("size-5 md:size-10 rounded-full border border-neutral-200 flex items-center justify-center shrink-0 hover:bg-neutral-100 transition duration-200 active:scale-90",className)}
+        {...rest}>
+            {children}
+        </button>
+    )
+}
+export const CardSkeleton = ({
+    className,
+    children 
+}:{
+    className?:string,
+    children?:React.ReactNode
+}
+) =>{
+    return(
+        <div className="min-h-40 md:min-h-60">{children}</div>
+    )
+}
